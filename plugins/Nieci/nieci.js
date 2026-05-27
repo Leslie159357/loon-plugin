7ad383da11255ccd25bbcf35788bdf14eebd3bf7
Leslie159357 <156227740+Leslie159357@users.noreply.github.com>
2026-05-27 11:47:39 +0000

    fix: var替换let/const, forEach改for循环兼容Loon

--- plugins/Nieci/nieci.js
+++ plugins/Nieci/nieci.js
@@ -2,17 +2,17 @@
 // 拦截API: www.kwhat.fun
 // 功能: 解锁会员 + 罐头积分
 
-const url = $request.url;
-const method = $request.method;
-const isResponse = typeof $response !== 'undefined';
+var url = $request.url;
+var method = $request.method;
+var isResponse = typeof $response !== 'undefined';
 
 if (isResponse) {
-  let body = $response.body;
+  var body = $response.body;
   
   try {
     // POST /nieci/user/getUserInfo - 会员信息
-    if (url.includes('/nieci/user/getUserInfo') && method === 'POST') {
-      let obj = JSON.parse(body);
+    if (url.indexOf('/nieci/user/getUserInfo') !== -1 && method === 'POST') {
+      var obj = JSON.parse(body);
       if (obj.code === 200 && obj.data) {
         obj.data.expireTime = "2099-12-31T23:59:59.000+00:00";
         obj.data.userType = 1;
@@ -24,8 +24,8 @@ if (isResponse) {
     }
     
     // GET /nieci/score/balance - 罐头积分
-    else if (url.includes('/nieci/score/balance') && method === 'GET') {
-      let obj = JSON.parse(body);
+    else if (url.indexOf('/nieci/score/balance') !== -1 && method === 'GET') {
+      var obj = JSON.parse(body);
       if (obj.code === 200) {
         obj.data = 999999;
         console.log('nieci: 积分修改成功');
@@ -34,8 +34,8 @@ if (isResponse) {
     }
     
     // POST /nieci/user/getBindIdol - 绑定数量
-    else if (url.includes('/nieci/user/getBindIdol') && method === 'POST') {
-      let obj = JSON.parse(body);
+    else if (url.indexOf('/nieci/user/getBindIdol') !== -1 && method === 'POST') {
+      var obj = JSON.parse(body);
       if (obj.code === 200 && obj.data) {
         obj.data.limitNum = 9999;
         console.log('nieci: 绑定数量解锁');
@@ -44,19 +44,19 @@ if (isResponse) {
     }
     
     // POST /nieci/order/getProductDetail-apple - 商品价格改为0
-    else if (url.includes('/nieci/order/getProductDetail-apple') && method === 'POST') {
-      let obj = JSON.parse(body);
+    else if (url.indexOf('/nieci/order/getProductDetail-apple') !== -1 && method === 'POST') {
+      var obj = JSON.parse(body);
       if (obj.code === 200 && obj.data) {
         if (obj.data.subscriptionList) {
-          obj.data.subscriptionList.forEach(item => {
-            item.payNum = 0;
-            item.originalPrice = 0;
-          });
+          for (var i = 0; i < obj.data.subscriptionList.length; i++) {
+            obj.data.subscriptionList[i].payNum = 0;
+            obj.data.subscriptionList[i].originalPrice = 0;
+          }
         }
         if (obj.data.consumeList) {
-          obj.data.consumeList.forEach(item => {
-            item.payNum = 0;
-          });
+          for (var i = 0; i < obj.data.consumeList.length; i++) {
+            obj.data.consumeList[i].payNum = 0;
+          }
         }
         console.log('nieci: 商品价格改为0');
       }
@@ -64,13 +64,13 @@ if (isResponse) {
     }
     
     // POST /nieci/user/getNotice - 标记所有公告已读
-    else if (url.includes('/nieci/user/getNotice') && method === 'POST') {
-      let obj = JSON.parse(body);
+    else if (url.indexOf('/nieci/user/getNotice') !== -1 && method === 'POST') {
+      var obj = JSON.parse(body);
       if (obj.code === 200 && obj.data) {
         if (obj.data.systemNoticeDTOs) {
-          obj.data.systemNoticeDTOs.forEach(item => {
-            item.ifChecked = true;
-          });
+          for (var i = 0; i < obj.data.systemNoticeDTOs.length; i++) {
+            obj.data.systemNoticeDTOs[i].ifChecked = true;
+          }
         }
         obj.data.uncheckedNum = 0;
       }
