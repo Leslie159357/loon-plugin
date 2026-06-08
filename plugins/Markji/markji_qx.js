93a098e80dc8f6e1d2cdd40601f151970214fc79
Leslie159357 <leslie@example.com>
2026-06-08 14:57:36 +0800

    Markji: QX script v1.2 final - simple flat structure

--- plugins/Markji/markji_qx.js
+++ plugins/Markji/markji_qx.js
@@ -1,5 +1,5 @@
 // ==Quantumult X==
-// @name         墨墨记忆卡 专业版解锁 v1.1
+// @name         墨墨记忆卡 专业版解锁 v1.2
 // @description  解锁墨墨记忆卡专业版所有功能 - 修改 /api/v2/system/check 响应
 // @version      1.2
 // @author       Minis
@@ -27,38 +27,27 @@ try {
         $done({});
     }
     
-    // 专业版解锁
     obj.data.plus_info = {
         "is_plus": true,
         "plus_expires_time": "2099-12-31T23:59:59.000Z",
         "is_lifetime": true
     };
-    
-    // 已付费标记
     obj.data.has_paid = true;
-    
-    // 学习限制解除
     obj.data.study_limit_info = {
         "day_new_limit": 9999,
         "day_review_limit": 99999,
         "new_affected_by_review_limit": false
     };
-    
-    // 牌组配额
     obj.data.study_info.private_deck_quota = 99999;
     obj.data.study_info.public_deck_quota = 99999;
     obj.data.study_info.replenish_card_count = 999;
     obj.data.study_info.available_mark = 999999;
     obj.data.study_info.free_mark = 999999;
     obj.data.study_info.paid_mark = 999999;
-    
-    // 卡片定价
     obj.data.card_price_enabled = true;
     obj.data.card_price_min_limit = 0;
     obj.data.card_price_max_limit = 999;
     obj.data.card_price_study_users_limit = 99999;
-    
-    // 其他限制
     obj.data.is_deck_max_limit_reached = false;
     
     $done({body: JSON.stringify(obj)});
